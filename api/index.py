48173f487e47e71683d3c415bf319596e94e2d73
Surya Kumar Srivastava <srivastavasurya@zohomail.in>
2026-01-02 22:29:52 +0530

    Add serverless-wsgi adapter for Vercel

--- api/index.py
+++ api/index.py
@@ -1,8 +1,9 @@
 # api/index.py
 
+from serverless_wsgi import handle_request
 from app import create_app
 
 app = create_app()
 
-def handler(request, context):
-    return app(request, context)
+def handler(event, context):
+    return handle_request(app, event, context)
