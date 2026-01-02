f52f1f7e6c399bd33ac115a17fe1d995eca04d6d
Surya Kumar Srivastava <srivastavasurya@zohomail.in>
2026-01-02 22:35:25 +0530

    updating

--- api/index.py
+++ api/index.py
@@ -1,9 +1,6 @@
 # api/index.py
 
-from serverless_wsgi import handle_request
-from app import create_app
+from app import app
 
-app = create_app()
-
-def handler(event, context):
-    return handle_request(app, event, context)
+# Vercel automatically uses this
+handler = app
